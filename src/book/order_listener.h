f945cb45046c73b1a811e5664b99227ad397cc69
iamtheschmitzer <schmitzj@ociweb.com>
2012-12-14 15:43:53 -0600

    Make order_listener accept references.

--- src/book/order_listener.h
+++ src/book/order_listener.h
@@ -9,25 +9,25 @@ template <class OrderPtr = Order*>
 class OrderListener {
 public:
   /// @brief callback for an order accept
-  virtual void on_accept(OrderPtr order) = 0;
+  virtual void on_accept(const OrderPtr& order) = 0;
 
   /// @brief callback for an order reject
-  virtual void on_reject(OrderPtr order, const char* reason) = 0;
+  virtual void on_reject(const OrderPtr& order, const char* reason) = 0;
 
   /// @brief callback for an order fill
-  virtual void on_fill(OrderPtr order, Quantity fill_qty, Cost cost) = 0;
+  virtual void on_fill(const OrderPtr& order, Quantity fill_qty, Cost cost) = 0;
 
   /// @brief callback for an order cancellation
-  virtual void on_cancel(OrderPtr order) = 0;
+  virtual void on_cancel(const OrderPtr& order) = 0;
 
   /// @brief callback for an order cancel rejection
-  virtual void on_cancel_reject(OrderPtr order, const char* reason) = 0;
+  virtual void on_cancel_reject(const OrderPtr& order, const char* reason) = 0;
 
   /// @brief callback for an order replace
-  virtual void on_replace(OrderPtr order) = 0;
+  virtual void on_replace(const OrderPtr& order) = 0;
 
   /// @brief callback for an order replace rejection
-  virtual void on_replace_reject(OrderPtr order, const char* reason) = 0;
+  virtual void on_replace_reject(const OrderPtr& order, const char* reason) = 0;
 };
 
 } }
