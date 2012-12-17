40052e4d95149e51a2d6410b34b49b1b8a028556
iamtheschmitzer <schmitzj@ociweb.com>
2012-12-17 14:25:10 -0600

    Test ask depth change stamps.

--- test/unit/ut_depth.cpp
+++ test/unit/ut_depth.cpp
@@ -410,14 +410,20 @@ BOOST_AUTO_TEST_CASE(TestAddAsk)
   depth.add_ask(1234, 100);
   const DepthLevel* first_ask = depth.asks();
   BOOST_REQUIRE(verify_level(first_ask, 1234, 1, 100));
+  ChangedChecker cc(depth);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
 }
 
 BOOST_AUTO_TEST_CASE(TestAddAsks)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1234, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1234, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 0, 0, 0, 0));
   depth.add_ask(1234, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 0, 0, 0, 0));
   const DepthLevel* first_ask = depth.asks();
   BOOST_REQUIRE(verify_level(first_ask, 1234, 3, 600));
 }
@@ -425,10 +431,15 @@ BOOST_AUTO_TEST_CASE(TestAddAsks)
 BOOST_AUTO_TEST_CASE(TestAppendAskLevels)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1236, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 0, 0, 0));
   depth.add_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 3, 3, 0, 0));
   depth.add_ask(1235, 400);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 4, 3, 0, 0));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1232, 1, 100));
   BOOST_REQUIRE(verify_level(ask, 1235, 2, 600));
@@ -438,11 +449,17 @@ BOOST_AUTO_TEST_CASE(TestAppendAskLevels)
 BOOST_AUTO_TEST_CASE(TestInsertAskLevels)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1234, 800);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 0, 0, 0));
   depth.add_ask(1236, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 3, 0, 0));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 4, 4, 0));
   depth.add_ask(1234, 900);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 5, 4, 4, 0));
   depth.add_ask(1231, 700);
   depth.add_ask(1235, 400);
   depth.add_ask(1231, 500);
@@ -457,16 +474,27 @@ BOOST_AUTO_TEST_CASE(TestInsertAskLevels)
 BOOST_AUTO_TEST_CASE(TestInsertAskLevelsPast5)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1234, 800);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 0, 0, 0));
   depth.add_ask(1236, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 3, 0, 0));
   depth.add_ask(1231, 700);
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 4, 4, 4, 0));
   depth.add_ask(1234, 900);
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 4, 5, 4, 0));
   depth.add_ask(1235, 400);
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 4, 5, 6, 6));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 4, 5, 7, 6));
   depth.add_ask(1231, 500);
+  BOOST_REQUIRE(cc.verify_ask_stamps(8, 4, 5, 7, 6));
   depth.add_ask(1230, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(9, 9, 9, 9, 9));
   depth.add_ask(1229, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(10, 10, 10, 10, 10));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1229, 1,  200));
   BOOST_REQUIRE(verify_level(ask, 1230, 1,  200));
@@ -478,22 +506,35 @@ BOOST_AUTO_TEST_CASE(TestInsertAskLevelsPast5)
 BOOST_AUTO_TEST_CASE(TestInsertAskLevelsTruncate5)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1234, 800);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 0, 0, 0));
   depth.add_ask(1236, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 3, 0, 0));
   depth.add_ask(1231, 700);
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 4, 4, 4, 0));
   depth.add_ask(1234, 900);
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 4, 5, 4, 0));
   depth.add_ask(1235, 400);
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 4, 5, 6, 6));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 4, 5, 7, 6));
   depth.add_ask(1231, 500);
+  BOOST_REQUIRE(cc.verify_ask_stamps(8, 4, 5, 7, 6));
   depth.add_ask(1230, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(9, 9, 9, 9, 9));
   depth.add_ask(1238, 200);
-  depth.add_ask(1238, 250);
+  BOOST_REQUIRE(cc.verify_ask_stamps(9, 9, 9, 9, 9));
+  depth.add_ask(1232, 250);
+  BOOST_REQUIRE(cc.verify_ask_stamps(9, 9, 10, 9, 9));
   depth.add_ask(1237, 500);
+  BOOST_REQUIRE(cc.verify_ask_stamps(9, 9, 10, 9, 9));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1230, 1,  200));
   BOOST_REQUIRE(verify_level(ask, 1231, 2, 1200));
-  BOOST_REQUIRE(verify_level(ask, 1232, 1,  100));
+  BOOST_REQUIRE(verify_level(ask, 1232, 2,  350));
   BOOST_REQUIRE(verify_level(ask, 1234, 2, 1700));
   BOOST_REQUIRE(verify_level(ask, 1235, 2,  600));
 }
@@ -501,8 +542,11 @@ BOOST_AUTO_TEST_CASE(TestInsertAskLevelsTruncate5)
 BOOST_AUTO_TEST_CASE(TestCloseAsk)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1234, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1234, 500);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 0, 0, 0, 0));
   BOOST_REQUIRE(!depth.close_ask(1234, 300)); // Does not erase
   const DepthLevel* first_ask = depth.asks();
   BOOST_REQUIRE(verify_level(first_ask, 1234, 1, 500));
@@ -511,11 +555,17 @@ BOOST_AUTO_TEST_CASE(TestCloseAsk)
 BOOST_AUTO_TEST_CASE(TestCloseEraseAsk)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1233, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1234, 500);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 2, 0, 0, 0));
   depth.add_ask(1233, 400);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 2, 0, 0, 0));
   BOOST_REQUIRE(!depth.close_ask(1233, 300)); // Does not erase
+  BOOST_REQUIRE(cc.verify_ask_stamps(4, 2, 0, 0, 0));
   BOOST_REQUIRE(depth.close_ask(1233, 400)); // Erase
+  BOOST_REQUIRE(cc.verify_ask_stamps(5, 5, 0, 0, 0));
   const DepthLevel* first_ask = depth.asks();
   BOOST_REQUIRE(verify_level(first_ask, 1234, 1, 500));
 }
@@ -523,9 +573,13 @@ BOOST_AUTO_TEST_CASE(TestCloseEraseAsk)
 BOOST_AUTO_TEST_CASE(TestAddCloseAddAsk)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1234, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.close_ask(1234, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 0, 0, 0, 0));
   depth.add_ask(1233, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 0, 0, 0, 0));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1233, 1, 200));
   BOOST_REQUIRE(verify_level(ask, 0, 0, 0));
@@ -534,9 +588,13 @@ BOOST_AUTO_TEST_CASE(TestAddCloseAddAsk)
 BOOST_AUTO_TEST_CASE(TestAddCloseAddHigherAsk)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1234, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.close_ask(1234, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 0, 0, 0, 0));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 0, 0, 0, 0));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1235, 1, 200));
   BOOST_REQUIRE(verify_level(ask, 0, 0, 0));
@@ -545,17 +603,29 @@ BOOST_AUTO_TEST_CASE(TestAddCloseAddHigherAsk)
 BOOST_AUTO_TEST_CASE(TestCloseAsksFreeLevels)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1234, 800);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 0, 0, 0));
   depth.add_ask(1236, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 3, 0, 0));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 4, 4, 0));
   depth.add_ask(1234, 900);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 5, 4, 4, 0));
   depth.add_ask(1231, 700);
+  BOOST_REQUIRE(cc.verify_ask_stamps(6, 6, 6, 6, 6));
   depth.add_ask(1235, 400);
+  BOOST_REQUIRE(cc.verify_ask_stamps(6, 6, 6, 7, 6));
   depth.add_ask(1231, 500);
-  depth.close_ask(1234, 900);
-  depth.close_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(8, 6, 6, 7, 6));
+  depth.close_ask(1234, 900); // does not erase
+  BOOST_REQUIRE(cc.verify_ask_stamps(8, 6, 9, 7, 6));
+  depth.close_ask(1232, 100); // erase
+  BOOST_REQUIRE(cc.verify_ask_stamps(8, 10, 10, 10, 10));
   depth.close_ask(1236, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(8, 10, 10, 11, 10));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1231, 2, 1200));
   BOOST_REQUIRE(verify_level(ask, 1234, 1,  800));
@@ -576,13 +646,21 @@ BOOST_AUTO_TEST_CASE(TestCloseAsksFreeLevels)
 BOOST_AUTO_TEST_CASE(TestIncreaseAsk)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1236, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 0, 0, 0));
   depth.add_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 3, 3, 0, 0));
   depth.add_ask(1235, 400);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 4, 3, 0, 0));
   depth.increase_ask(1232, 37);
+  BOOST_REQUIRE(cc.verify_ask_stamps(5, 4, 3, 0, 0));
   depth.increase_ask(1232, 41);
+  BOOST_REQUIRE(cc.verify_ask_stamps(6, 4, 3, 0, 0));
   depth.increase_ask(1235, 201);
+  BOOST_REQUIRE(cc.verify_ask_stamps(6, 7, 3, 0, 0));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1232, 1, 178));
   BOOST_REQUIRE(verify_level(ask, 1235, 2, 801));
@@ -592,13 +670,21 @@ BOOST_AUTO_TEST_CASE(TestIncreaseAsk)
 BOOST_AUTO_TEST_CASE(TestDecreaseAsk)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1236, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 0, 0, 0));
   depth.add_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 3, 3, 0, 0));
   depth.add_ask(1235, 400);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 4, 3, 0, 0));
   depth.decrease_ask(1236, 37);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 4, 5, 0, 0));
   depth.decrease_ask(1236, 41);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 4, 6, 0, 0));
   depth.decrease_ask(1235, 201);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 7, 6, 0, 0));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1232, 1, 100));
   BOOST_REQUIRE(verify_level(ask, 1235, 2, 399));
@@ -608,15 +694,25 @@ BOOST_AUTO_TEST_CASE(TestDecreaseAsk)
 BOOST_AUTO_TEST_CASE(TestIncreaseDecreaseAsk)
 {
   SizedDepth depth;
+  ChangedChecker cc(depth);
   depth.add_ask(1236, 300);
+  BOOST_REQUIRE(cc.verify_ask_stamps(1, 0, 0, 0, 0));
   depth.add_ask(1235, 200);
+  BOOST_REQUIRE(cc.verify_ask_stamps(2, 2, 0, 0, 0));
   depth.add_ask(1232, 100);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 3, 3, 0, 0));
   depth.add_ask(1235, 400);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 4, 3, 0, 0));
   depth.increase_ask(1236, 37);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 4, 5, 0, 0));
   depth.decrease_ask(1235, 41);
+  BOOST_REQUIRE(cc.verify_ask_stamps(3, 6, 5, 0, 0));
   depth.increase_ask(1232, 51);
+  BOOST_REQUIRE(cc.verify_ask_stamps(7, 6, 5, 0, 0));
   depth.decrease_ask(1236, 41);
+  BOOST_REQUIRE(cc.verify_ask_stamps(7, 6, 8, 0, 0));
   depth.increase_ask(1236, 201);
+  BOOST_REQUIRE(cc.verify_ask_stamps(7, 6, 9, 0, 0));
   const DepthLevel* ask = depth.asks();
   BOOST_REQUIRE(verify_level(ask, 1232, 1, 151));
   BOOST_REQUIRE(verify_level(ask, 1235, 2, 559));
